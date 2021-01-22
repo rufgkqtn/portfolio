0ec0c3d7ffce65d0c078a7bd31d6f3debcad334a
rufgkqtn <71644477+rufgkqtn@naver.coom>
2021-01-22 18:48:06 +0900

    JS: Arrow up

--- main.js
+++ main.js
@@ -44,3 +44,18 @@ function scrollIntoView(selector){
     scrollTo.scrollIntoView({ behavior: 'smooth' });
 }
 
+//show arrow up button when scrolling down
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', ()=>{
+    if(window.scrollY> homeHeight /2){
+        arrowUp.classList.add('visible');
+    }
+    else{
+        arrowUp.classList.remove('visible');
+    }
+});
+
+//Handle click on the arrow up button
+arrowUp.addEventListener('click', ()=>{
+    scrollIntoView('#Home');
+})
